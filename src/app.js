1becf8451a01759d52088561f1f66f305d248f6a
Robeen M <maharjanrobeen@gmail.com>
2022-03-31 20:18:12 -0700

    App.js now can remove all div elements nested in the copy contianer

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
-const originals = document.querySelector(".original-container")
 const cloneRecipient = document.querySelector(".copy-container")
+const clearButton = document.querySelector(".copies button")
+const originals = document.querySelector(".original-container")
 
+clearButton.addEventListener('click', removeAllNode)
 originals.addEventListener('click', itemCopy)
+
 
 function itemCopy(event) {
     if (event.target != originals) {
         const to_clone = event.target.closest("div")
         const clone = to_clone.cloneNode(true)
         cloneRecipient.append(clone)
+    }
+}
+
+
+function removeAllNode() {
+    while (cloneRecipient.firstChild) {
+        cloneRecipient.removeChild(cloneRecipient.firstChild)
     }
 }
